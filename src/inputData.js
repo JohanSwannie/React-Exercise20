822dcb84371a32fce90b8c5ed48b94ab5876c290
Johan Swan <jswannieman@gmail.com>
2024-03-03 09:41:03 +1300

    Commit inputData JS Component

---
+++ src/inputData.js
@@ -0,0 +1,68 @@
+export const data = [
+  {
+    fullname: "Richard Swan",
+    age: 59,
+    career: "Executive Chef",
+    salary: "108,000.00",
+  },
+  {
+    fullname: "Julie Swan",
+    age: 58,
+    career: "Artist",
+    salary: "70,000.00",
+  },
+  {
+    fullname: "Johan Swan",
+    age: 60,
+    career: "Senior Software Engineer",
+    salary: "140,000.00",
+  },
+  {
+    fullname: "Chris Smit",
+    age: 62,
+    career: "Regimental Sergeant Major",
+    salary: "115,000.00",
+  },
+  {
+    fullname: "Frikkie Smit",
+    age: 61,
+    career: "Actor",
+    salary: "550,000.00",
+  },
+  {
+    fullname: "James Mower",
+    age: 56,
+    career: "Powerlifter",
+    salary: "125,000.00",
+  },
+  {
+    fullname: "Tommie Theron",
+    age: 58,
+    career: "Civil Engineer",
+    salary: "135,000.00",
+  },
+  {
+    fullname: "Leon Viljoen",
+    age: 55,
+    career: "Metallurgical engineer",
+    salary: "250,000.00",
+  },
+  {
+    fullname: "Sonja Golden",
+    age: 54,
+    career: "Business Owner",
+    salary: "875,000.00",
+  },
+  {
+    fullname: "Roxanne Brown",
+    age: 49,
+    career: "Artist",
+    salary: "90,000.00",
+  },
+  {
+    fullname: "Danny Sidaway",
+    age: 50,
+    career: "Project Manager",
+    salary: "160,000.00",
+  },
+];
